74facdf0e957f09614c3d668776f420f2dcc9cdb
worker <3327879797@qq.com>
2021-02-28 21:35:16 +0800

    引入parcel

--- button.js
+++
@@ -1,5 +0,0 @@
-Vue.component('c-button', {
-    template: `
-        <button class ="c-btn">提交</button>
-    `
-})
---
+++ src/button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button class="c-btn">提交</button>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+.c-btn {
+  height: var(--btn-height);
+  padding: 0 1em;
+  background-color: var(--btn-bg);
+  color: var(--font-color);
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  &:hover {
+    background-color: var(--btn-hover-bg);
+  }
+  &:active {
+    outline: none;
+    background-color: var(--btn-active-bg);
+  }
+}
+</style>
